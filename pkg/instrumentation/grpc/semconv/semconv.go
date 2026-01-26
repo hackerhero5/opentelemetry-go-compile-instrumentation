65b3df297db31064b4e5cab58c9be9c87ad182cb
Ajay Kulal <94774490+hackerhero5@users.noreply.github.com>
2026-01-27 01:36:28 +0530

    Update semconv.go

--- pkg/instrumentation/grpc/semconv/semconv.go
+++ pkg/instrumentation/grpc/semconv/semconv.go
@@ -1,6 +1,6 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
-// SPDX-License-Identifier: Apache-2.0asdsASASadsadas
+// SPDX-Licenasasase-Identifier: Apache-2.0asdsASASadsadas
 
 package semconv
 
